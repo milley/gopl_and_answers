acb69f937742d13aba0f92a584a6527a3d1bc112
milley <milley.gm@gmail.com>
2018-12-31 18:14:05 +0800

    complete gensqlbytablename

--- ch4/gensqlbytablename/main.go
+++ ch4/gensqlbytablename/main.go
@@ -11,6 +11,9 @@ var latnIds = [...]string{"290", "910", "911", "912", "913", "914", "915", "916"
 // GenDropTable generator drop table
 const GenDropTable string = "DROP TABLE "
 
+// GenTruncateTable generator truncate table
+const GenTruncateTable string = "TRUNCATE "
+
 // GenSelectTable generator select table
 const GenSelectTable string = "SELECT * FROM "
 
@@ -20,6 +23,9 @@ const UnionAll string = "UNION ALL"
 // Space space
 const Space string = " "
 
+// Wrap wrap
+const Wrap string = "\r\n"
+
 // Semicolon semicolon
 const Semicolon string = ";"
 
@@ -28,7 +34,6 @@ const PlaceHolders string = "${latnId}"
 
 func genSelectSQL(tableName string) (sql string, ok bool) {
 	if strings.Index(tableName, PlaceHolders) != -1 {
-		//sqlSlice := make([]string, 10)
 		var sqlSlice []string
 		for index, latnID := range latnIds {
 			vSQL := ""
@@ -40,7 +45,7 @@ func genSelectSQL(tableName string) (sql string, ok bool) {
 			sqlSlice = append(sqlSlice, vSQL)
 		}
 		if sqlSlice != nil {
-			sql := strings.Join(sqlSlice, Space)
+			sql := strings.Join(sqlSlice, Wrap)
 			return sql, true
 		} else {
 			return "", false
@@ -49,22 +54,60 @@ func genSelectSQL(tableName string) (sql string, ok bool) {
 	return "", false
 }
 
+func genDropOrTruncateSQL(arg string, tableName string) (sql string, ok bool) {
+	var sqlSlice []string
+
+	for _, latnID := range latnIds {
+		vSQL := ""
+		switch arg {
+		case "DROP":
+			vSQL = GenDropTable + strings.Replace(tableName, PlaceHolders, latnID, -1) + Semicolon
+		case "TRUNCATE":
+			vSQL = GenTruncateTable + strings.Replace(tableName, PlaceHolders, latnID, -1) + Semicolon
+		default:
+
+		}
+		if len(vSQL) > 0 {
+			sqlSlice = append(sqlSlice, vSQL)
+		}
+
+	}
+	if sqlSlice != nil {
+		sql := strings.Join(sqlSlice, Wrap)
+		return sql, true
+	}
+
+	return "", false
+}
+
 func main() {
 	if len(os.Args) != 3 {
 		fmt.Printf("The input arguments is error: %v\n", os.Args)
-		fmt.Printf("The Usage: gensqlbytable select/drop tablename")
+		fmt.Printf("The Usage: gensqlbytable select/drop/truncate tablename")
 		os.Exit(1)
 	}
 
 	operator := os.Args[1]
+	upperOperator := strings.ToUpper(operator)
 	tableName := os.Args[2]
 
-	switch strings.ToUpper(operator) {
-	case "SELECT":
+	switch {
+	case upperOperator == "SELECT":
 		if sql, ok := genSelectSQL(tableName); len(sql) != 0 && ok {
 			fmt.Println(sql)
 		} else {
 			fmt.Printf("tablename can not format :%s", tableName)
 		}
+	case (upperOperator == "DROP" || upperOperator == "TRUNCATE"):
+		if sql, ok := genDropOrTruncateSQL(upperOperator, tableName); len(sql) != 0 && ok {
+			fmt.Println(sql)
+		} else {
+			fmt.Printf("tablename can not format :%s", tableName)
+		}
+	default:
+		fmt.Printf("The args[1]: %s is not supply!\n", operator)
+		os.Exit(1)
 	}
+
+	os.Exit(0)
 }
